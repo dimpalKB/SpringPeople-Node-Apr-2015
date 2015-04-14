79cf42167118d9671131547a6f1bbbdfde14e207
tkmagesh <tkmagesh77@gmail.com>
2015-04-14 22:13:01 +0530

    Making the server both web and app server

--- Day-02/01-FileOperations/printFile.js
+++ Day-02/01-FileOperations/printFile.js
@@ -1,5 +1,19 @@
 var fs = require("fs");
-fs.readFile("data.txt", {encoding : "utf8"}, function(err, data){
+/*fs.readFile("data.txt", {encoding : "utf8"}, function(err, data){
+    console.log(data);
+});*/
+
+var stream = fs.createReadStream("data.txt", {encoding : "utf8"});
+var readCount = 0;
+var totalFileSize = 0;
+stream.on("data", function(data){
+    totalFileSize += data.length;
+    ++readCount;
     console.log(data);
 });
+stream.on("end", function(){
+    console.log("file size = ", totalFileSize);
+    console.log("Read count = ", readCount);
+});
+console.log("reading file");
 
--- Day-02/02-WebServer/server.js
+++ Day-02/02-WebServer/server.js
@@ -1,22 +1,55 @@
-var http = require("http");
-var fs = require("fs");
-var path = require("path");
+var http = require("http"),
+    fs = require("fs"),
+    path = require("path"),
+    url = require('url');
+
+
+/*
+1. check if the request is for a static resource
+2. if yes, serve the resource (handle non existence scenario)
+3. else, if the request is for "/calculator"
+4. if yes, parse the req url, extract the data, calculate and serve the response
+5. else send 404 status code
+*/
+var staticResourceExtns = [".html", ".txt", ".js", ".jpg",".png",".ico",".css"]
+function isStatic(resource){
+    var resourceExtn = path.extname(resource);
+    return staticResourceExtns.indexOf(resourceExtn) !== -1;
+}
 
 var server = http.createServer(function(req, res){
-    var resource = path.join(__dirname , req.url);
-    console.log(resource);
-    if (fs.existsSync(resource)){
-        fs.readFile(resource, {encoding : "utf8"}, function(err, data){
-            if (err){
+    var requestedUrl = url.parse(req.url)
+    if (isStatic(requestedUrl.pathname)){
+        var resource = path.join(__dirname , requestedUrl.pathname);
+        console.log(resource);
+        if (fs.existsSync(resource)){
+            /*fs.readFile(resource, {encoding : "utf8"}, function(err, data){
+                if (err){
+                    res.statusCode = 404;
+                    res.end();
+                } else {
+                    res.write(data);
+                    res.end();
+                }
+            });*/
+            var stream = fs.createReadStream(resource, {encoding : "utf8"});
+           /* stream.on("data", function(data){
+                res.write(data);
+            });
+            stream.on("end", function(){
+                res.end();
+            });
+            stream.on("error", function(){
                 res.statusCode = 404;
                 res.end();
-            } else {
-                res.write(data);
-                res.end();
-            }
-        });
+            });*/
+            stream.pipe(res);
+        } else {
+            res.statusCode = 404;
+            res.end();
+        }
     } else {
-        res.statusCode = 404;
+        res.write("The requested resource is not a static resource");
         res.end();
     }
 });
